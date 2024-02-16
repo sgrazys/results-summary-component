4c9b82374ba142a36a0d7de871cda37c434318c1
sgrazys <s.grazys@gmail.com>
2024-02-16 23:27:26 +0200

    summary component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import data from '../data/data.json';
+import './App.css';
 import SummaryComponent from './SummaryComponent';
 import Results from './Results';
 import Abilities from './Abilities';
@@ -7,10 +8,12 @@ function App() {
 	const score = Math.floor(data.reduce((prev, cur) => prev + cur.score, 0) / data.length);
 
 	return (
-		<SummaryComponent>
-			<Results score={score} />
-			<Abilities data={data} />
-		</SummaryComponent>
+		<div className='app'>
+			<SummaryComponent>
+				<Results score={score} />
+				<Abilities data={data} />
+			</SummaryComponent>
+		</div>
 	);
 }
 
